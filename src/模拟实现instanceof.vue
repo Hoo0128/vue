8d7be029e991c25a936e53d4488d579e94e78274
Hoo0128 <945174732@qq.com>
2022-07-02 17:10:49 +0800

    模拟实现instanceof

---
+++ src/模拟实现instanceof.vue
@@ -0,0 +1,27 @@
+<template> 
+ <div></div> 
+ </template> 
+   <script> 
+ //2. 模拟实现instanceof 
+ const _instanceof = (left, right) => { 
+ right = right.prototype; 
+ left = left.__proto__; 
+ while (true) { 
+ if (left == null) { 
+ return false; 
+ } 
+ if (left == right) { 
+ return true; 
+ } 
+ left = left.__proto__; 
+ } 
+ }; 
+ const arr = new Array(); 
+ const obj = new Object(); 
+ console.log(_instanceof(arr, Object)); 
+ console.log(_instanceof(obj, Array)); 
+ console.log(arr instanceof Object); 
+ console.log(obj instanceof Object); 
+ export default {}; 
+ </script> 
+   <style></style> 
