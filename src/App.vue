a86c628271cef8f747f66ae0e0feb8434ab22ee0
Hoo0128 <945174732@qq.com>
2022-07-05 15:14:30 +0800

    图书馆

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,102 @@
 <template>
-  <div id="main">
-    <Header @search="searchFn"></Header>
-    <Main :data="list" @del="delFn" @detail="detailFn"></Main>
-    <div id="foolter">
-      <Footer @add="addFn" ref="myFooter"></Footer>
-      <Detail :book="book"></Detail>
+  <div id="app">
+    <div style="margin: 40px auto">
+      <input
+        type="text"
+        class="form-control"
+        style="width: 220px"
+        placeholder="搜索-书本名称"
+        @keydown.enter="enterFn"
+        v-model="bookname"
+      />
+
+      <button
+        class="btn btn-primary"
+        style="margin-top: 20px"
+        @click="isShow = !isShow"
+      >
+        新增({{ isShow ? "隐藏" : "显示" }})
+      </button>
+      <AddBook v-if="isShow" @add="addFn" ref="addbook"></AddBook>
     </div>
+
+    <MyTable :list="list" @del="delFn" @detail="detailFn"></MyTable>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Main from './components/Main.vue';
-import Footer from './components/Footer.vue';
-import Detail from './components/Detail.vue';
+import MyTable from "./components/Table.vue";
+import AddBook from "./components/AddBook.vue";
+
 export default {
+  name: "App",
   data() {
     return {
       list: [],
-      book: [],
+      bookname: "",
+      isShow: false,
     };
   },
-  // 挂载后自动调用该方法
-  mounted() {
-    this.getBook();
+  components: {
+    MyTable,
+    AddBook,
+  },
+  created() {
+    this.getList();
   },
   methods: {
-    // 获取数据
-    getBook() {
+    enterFn() {
+      const params = {};
+      this.bookname ? (params.bookname = this.bookname) : "";
+      this.getList(params);
+    },
+    getList(params = {}) {
       this.$axios({
-        method: 'GET',
-        url: '/api/getbooks',
+        url: "/api/getbooks",
+        params: params,
       }).then((res) => {
-        const { data } = res;
-        this.list = data.data;
+        this.list = res.data.data;
       });
     },
-    // 根据搜索框获取书籍
-    searchFn(val) {
+    delFn(id) {
       this.$axios({
-        method: 'GET',
-        url: '/api/getbooks',
-        params: {
-          bookname: val,
+        url: "/api/delbook",
+        params: { id },
+      }).then(() => {
+        this.getList();
+      });
+    },
+    addFn(obj) {
+      this.$refs.addbook.disabled = true;
+      this.$axios({
+        url: "/api/addbook",
+        method: "POST",
+        data: {
+          ...obj,
+          appkey: "7250d3eb-18e1-41bc-8bb2-11483665535a",
         },
       }).then((res) => {
-        const { data } = res;
-        if (data.status !== 200) {
-          return alert('没有该书籍');
+        this.$refs.addbook.disabled = false;
+        this.$refs.addbook.clear();
+        alert(res.data.msg);
+        if (res.data.status == 200 || res.data.status == 201) {
+          this.getList();
         }
-        this.list = data.data;
       });
     },
-    // 新增数据
-    addFn(val) {
-      this.$axios({
-        method: 'POST',
-        url: '/api/addbook',
-        data: { ...val },
-      }).then((res) => {
-        const { data } = res;
-        if (data.status == 502) return alert(data.msg);
-        alert(data.msg);
-        this.$refs.myFooter.$refs.myBtn.removeAttribute('disabled');
-        this.getBook();
-      });
-    },
-    // 删除按钮
-    delFn(id) {
-      // console.log(id);
-      this.$axios({
-        method: 'GET',
-        url: '/api/delbook',
-        params: { id },
-      }).then((res) => {
-        const { data } = res;
-        alert(data.msg);
-        this.getBook();
-      });
-    },
-    // 详情按钮
     detailFn(id) {
       this.$axios({
-        method: 'GET',
-        url: '/api/getbooks',
+        url: "/api/getbooks",
         params: { id },
       }).then((res) => {
-        const { data } = res;
-        if (data.status !== 200) return alert('获取图书信息失败!');
-        this.book = data.data;
+        const info = res.data.data && res.data.data[0];
+        alert(
+          `作者：${info.author}; 出版社：${info.publisher}; 书名：${info.bookname}`
+        );
       });
     },
-  },
-  components: {
-    Header,
-    Main,
-    Footer,
-    Detail,
   },
 };
 </script>
 
-<style>
-#main {
-  padding: 100px;
-}
-#foolter {
-  display: flex;
-  justify-content: space-between;
-}
-</style>
+<style></style>
